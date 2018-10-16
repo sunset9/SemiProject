b92128614d18ca27e69c0bffd541e26f9aa9786b
sunset9 <haejin940@gmail.com>
2018-10-16 11:00:41 +0900

    fullcalender api 추가

--- Semi/src/controller/admin/AdminCommentListController.java
+++ Semi/src/controller/admin/AdminCommentListController.java
@@ -13,7 +13,7 @@ import service.plan.PlanServiceImpl;
 import service.stroy.StoryService;
 import service.stroy.StoryServiceImpl;
 
-@WebServlet("/stoty/comment/delete")
+@WebServlet("/stoty/comment/list")
 public class AdminCommentListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
