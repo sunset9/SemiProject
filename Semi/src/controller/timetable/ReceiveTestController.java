e5775920b2002f96fc9d45a1ec173268b95e13b9
sunset9 <haejin940@gmail.com>
2018-10-19 15:16:34 +0900

    1019 timetable 서버에서 넘어온 값으로 띄우기 구현 중(view 기능) - 해진

---
+++ Semi/src/controller/timetable/ReceiveTestController.java
@@ -0,0 +1,89 @@
+package controller.timetable;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import dto.timetable.Location;
+import dto.timetable.Timetable;
+
+
+@WebServlet("/timetable/recv")
+public class ReceiveTestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		
+		// -------------- jsp 에서 event리스트 받아오기 테스트 ---------
+		 GsonBuilder gsonBuilder = new GsonBuilder();
+         gsonBuilder.setDateFormat("yyyy-MM-dd hh:mm");
+         Gson gson = gsonBuilder.create();
+		
+		// --- events로 json형식으로 여러개 넘겨주는 경우
+//		String[] events = req.getParameterValues("events");
+//		for(String event:events) {
+//			Location loc = gson.fromJson(event, Location);
+//			System.out.println(loc);
+//		}
+		
+		// --- events로 리스트형식으로 하나 넘겨주는 경우
+		String events = req.getParameter("events");
+		System.out.println(events);
+		
+		Location[] objs = gson.fromJson(events, Location[].class);
+		List<Location> list = Arrays.asList(objs);
+
+		for(Location loc:list) {
+			System.out.println(loc);
+		}
+	
+		//-------------------- DB에 있는정보 뿌려주기-----------------------------
+		List<Timetable> ttbList = new ArrayList<Timetable>() ;
+		List<Location> locList = new ArrayList<Location>();
+		for(int i = 0 ; i<4; i++) {
+			Timetable ttb = new Timetable();
+			ttb.setStart_time(objs[i].getStart_date());
+			ttb.setEnd_time(objs[i].getEnd_date());
+			ttb.setLoc_idx(1);
+			
+			Location loc = new Location();
+			loc.setLoc_idx(1);
+			loc.setAddress(objs[i].getAddress());
+			loc.setPlace_name(objs[i].getPlace_name());
+			
+			ttbList.add(ttb);
+			locList.add(loc);
+		}
+		
+		
+		String ttbListStr = gson.toJson(ttbList);
+		String locListStr = gson.toJson(locList);
+		
+		System.out.println(gson.toJson(ttbListStr));
+		System.out.println(gson.toJson(locListStr));
+		
+		req.setAttribute("ttbList", ttbListStr);
+		req.setAttribute("locList", locListStr);
+		
+		req.getRequestDispatcher("/plan/timetable/timetable_recv.jsp").forward(req, resp);
+		
+	}
+}
--- Semi/src/dto/timetable/Location.java
+++ Semi/src/dto/timetable/Location.java
@@ -15,4 +15,57 @@ public class Location {
 	private String address;
 	
 	private Date start_date; // DB테이블엔 없는 컬럼, Timetable에 있는 정보
+	private Date end_date; // DB테이블엔 없는 컬럼, Timetable에 있는 정보
+	
+	
+	@Override
+	public String toString() {
+		return "Location [loc_idx=" + loc_idx + ", place_name=" + place_name + ", lat=" + lat + ", lon=" + lon
+				+ ", address=" + address + ", start_date=" + start_date + ", end_date=" + end_date + "]";
+	}
+	public int getLoc_idx() {
+		return loc_idx;
+	}
+	public void setLoc_idx(int loc_idx) {
+		this.loc_idx = loc_idx;
+	}
+	public String getPlace_name() {
+		return place_name;
+	}
+	public void setPlace_name(String place_name) {
+		this.place_name = place_name;
+	}
+	public float getLat() {
+		return lat;
+	}
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+	public float getLon() {
+		return lon;
+	}
+	public void setLon(float lon) {
+		this.lon = lon;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	
+	
+
 }
