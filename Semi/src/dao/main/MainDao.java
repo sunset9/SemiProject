f976db588866d100998361bb5c225d5bcc50041c
Majinbuu2 <nc1315@naver.com>
2018-10-25 12:30:24 +0900

    1025목 지은 메인컨트롤러에 플랜 연결하긔
    1.메인에서 새일정만들면 파라미터 받아서 /plan에 plan_idx넘겨주는거 추가

--- Semi/src/dao/main/MainDao.java
+++ Semi/src/dao/main/MainDao.java
@@ -2,11 +2,20 @@ package dao.main;
 
 import java.util.List;
 
+import dto.plan.Plan;
+import dto.user.User;
+
 public interface MainDao {
 
 	//추천콘텐츠 검색
-	List getRecommendedContents();
+	public List getRecommendedContents();
 	
 	//최신콘텐츠 검색
-	List getNewestContents();
+	public List getNewestContents();
+	
+	//새 일정 만들기
+	public void insertPlan(Plan param, User user_idx);
+	
+	//plan_idx 가져오기
+	public int getPlan_idx();
 }
